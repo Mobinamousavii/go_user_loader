8ec3ecd1d1b4b2f46983a15e7c7f300d8f777d16
Mobina Mousavi <mobina.mousavi@pintapin.local>
2026-01-05 00:46:18 +0330

    feat(config):added workers flag

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,6 +9,7 @@ import (
 type Config  struct{
 	Filepath string
 	Port int
+	Workers int
 }
 
 func Parse(args []string)(Config , error){
@@ -18,6 +19,8 @@ func Parse(args []string)(Config , error){
 
 	fs.IntVar(&cfg.Port, "port", 8080, "port the program listens on for incoming requests")
 	fs.StringVar(&cfg.Filepath, "file", "", "path to users file (.csv/.json)")
+	fs.IntVar(&cfg.Workers, "workers", 5, "Number of workers to process records concurrently")
+	
 
 	if err := fs.Parse(args); err != nil{
 		return Config{}, err
@@ -29,6 +32,10 @@ func Parse(args []string)(Config , error){
 
 	if cfg.Port <= 0 || cfg.Port > 65535 {
 		return Config{}, fmt.Errorf("invalid --port: %d", cfg.Port)
+	}
+
+	if cfg.Workers <= 0 {
+		return  Config{}, fmt.Errorf("invalid value for 'workers': %d. The number of workers must be greater than or equal to 1.", cfg.Workers)
 	}
 
 
